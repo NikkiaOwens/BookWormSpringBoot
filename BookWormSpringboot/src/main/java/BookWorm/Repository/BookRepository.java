dd38401921c0a9abc3eab0c0ae3ce8b015ba2373
NikkiaOwens <73052596+NikkiaOwens@users.noreply.github.com>
2021-01-22 19:58:04 -0500

    Delete BookWormSpringboot directory

--- BookWormSpringboot/src/main/java/BookWorm/Repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package BookWorm.Repository;
-
-import BookWorm.Model.Book;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends CrudRepository<Book, Long> {
-}
